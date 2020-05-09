4e1ac2687faa4ca7095ed75e426d31a788df9df5
feralonsomaccari <feralonsomaccari@gmail.com>
2020-05-09 16:26:48 -0300

    Debug problem with github pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import ListContainer from "./Components/List/ListContainer";
+// import ListContainer from "./Components/List/ListContainer";
 import "antd/dist/antd.css";
 
 function App() {
   return (
     <div >
-      <ListContainer></ListContainer>
+      PEPE
+      {/* <ListContainer></ListContainer> */}
     </div>
   );
 }
