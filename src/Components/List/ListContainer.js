1460175adf616757c7a349b6ccaeddd44abda41a
feralonsomaccari <feralonsomaccari@gmail.com>
2021-03-24 21:12:52 -0300

    Update favicon correct sizes and Fix env variable

--- src/Components/List/ListContainer.js
+++ src/Components/List/ListContainer.js
@@ -12,7 +12,7 @@ const ListContainer = () => {
   const [serverInfo, setServerInfo] = useState({});
 
   useEffect(() => {
-    fetch(process.env.SERVER_URL, {
+    fetch(process.env.REACT_APP_SERVER_URL, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
